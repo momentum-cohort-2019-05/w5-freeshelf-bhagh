f8f0b06f40c4719a23ec93408eadbd7cbb21ffec
bhagh <haghb1@gmail.com>
2019-06-25 20:46:26 -0400

    Add styling and list view

--- core/models.py
+++ core/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
--- core/views.py
+++ core/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse
+from django.views.generic import TemplateView
 
-# Create your views here.
+from core.models import Book, Author, Category
+
+class BookListView(generic.ListView):
+    model = Book
+
+    def get_category():
+        categories = Category.objects.all()
+        return categories
+
+class CategoryListView(generic.ListView):
+    model = Category
+
+
+from django.views.generic import TemplateView
+
+class MultipleModelView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+         context = super(MultipleModelView, self).get_context_data(**kwargs)
+         context['book'] = Book.objects.all()
+         context['category'] = Category.objects.all()
+         return context
